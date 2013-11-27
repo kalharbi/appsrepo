9fc54d21d2581fbad6d76075cb9d17402acdc8e3
Khalid Alharbi <Khalid3ahmed@gmail.com>
2013-11-26 20:03:00 -0700

    add logging level

--- public_features/logging.rb
+++ public_features/logging.rb
@@ -2,13 +2,27 @@ require 'logger'
 
 module Logging
   @log_dev = STDOUT
-  
+  @level = Logger::INFO
+
   def self.config_log(config)
-    log_dev = config['logdev']
-    if (log_dev != 'STDOUT')
-      log_file = File.open(log_dev, File::WRONLY | File::APPEND | File::CREAT)
-      @log_dev = log_file
+    if(!config['dev'].nil?)
+      log_dev = config['dev']
+      if (log_dev != 'STDOUT')
+        @log_dev = File.open(log_dev, File::WRONLY | File::APPEND | File::CREAT)
+      end
     end
+    
+    if (!config['level'].nil?)
+      level = config['level'].downcase
+      if(level == 'error')
+        @level = Logger::ERROR
+      elsif(level == 'warning')
+        @level = Logger::WARN
+      elsif(level == 'info')
+        @level = Logger::INFO
+      end
+    end
+
   end
 
   def logger
@@ -17,7 +31,7 @@ module Logging
   
   def self.logger
     @logger ||= Logger.new(@log_dev)
-    @logger.level = Logger::INFO
+    @logger.level = @level
     @logger
   end
 
