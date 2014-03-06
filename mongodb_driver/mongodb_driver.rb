ad9434c291280405ef80545f3a11e43f2319c8b6
Khalid Alharbi <Khalid3ahmed@gmail.com>
2014-03-05 20:14:16 -0700

    add find the top downloaded apps

--- mongodb_driver/mongodb_driver.rb
+++ mongodb_driver/mongodb_driver.rb
@@ -7,18 +7,20 @@ require_relative '../utils/logging'
 
 class MongodbDriver
 
-  @@usage = "Usage: #{$PROGRAM_NAME} {out_dir} [OPTIONS]"
+  @@usage = "Usage: #{$PROGRAM_NAME} {find_apps_with_permissions | find_apps_by_permission | find_top_apps} {out_dir} [OPTIONS]"
   DB_NAME = "apps"
   COLLECTION_NAME = "public"
   @collection
   @out_dir
   @per_name
-
+  @limit
+  
   attr_reader :host, :port
   
   def initialize
     @host = "localhost"
     @port = 27017
+    @limit = 500
   end
   
   private
@@ -51,21 +53,42 @@ class MongodbDriver
       Logging.logger.info("The app's description has been written to: #{out_file}")
     end
   end
+  
+  def find_top_apps
+    name_hd = "apk_name"
+    download_hd = "download_count"
+    out_file = File.join(@out_dir, "top_apps.txt")
+    File.open(name_hd, 'w') do |file| 
+      file.write(name + ", " + download_hd)
+      @collection.find({ "per" => { "$not" => { "$size" => 0 } } }, :fields => ["n", "dct"], :sort => ['dct', Mongo::DESCENDING], :limit => @limit).each do |doc|
+        name = doc["n"]
+        dct = doc["dct"]
+        file.write(name + ", " + dct)
+      end
+    end
+      Logging.logger.info("The top apps list has been written to: #{out_file}")
+  end
 
-
-  def start_main(out_dir)
+  def start_main(out_dir, cmd)
     beginning_time = Time.now
     if(!File.directory?(out_dir))
       puts "No such directory."
       abort(@@usage)
     end
     @out_dir = out_dir
-
     connect_mongodb
-    if(@per_name.nil?)
+    
+    if(cmd.eql? "find_apps_by_permission")
+      if(@per_name.nil?)
+        puts "Please indicate the permission name using the option -P."
+        abort(@@usage)
+      else
+        find_apps_with_permissions
+      end
+    elsif(cmd.eql? "find_apps_with_permissions")
       find_apps_with_permissions
-    else
-      find_apps_by_permission
+    elsif(cmd.eql? "find_top_apps")
+      find_top_apps
     end
 
     end_time = Time.now
@@ -99,6 +122,9 @@ class MongodbDriver
         opts.on('-P','--permission <name>', 'One valid Android permission name that the application needs.') do |per_name|
           @per_name = per_name
         end
+        opts.on('-l','--limit <value>', 'The number of documents in the result set.') do |limit_value|
+          @limit = limit
+        end
         opts.on('-v', '--verbose', 'Causes the tool to be verbose to explain what is being done.') do
           @verbose = true
         end
@@ -113,12 +139,28 @@ class MongodbDriver
       puts opt_parser.help()
       exit
     end
+    cmd = ""
     
-    if(args[0].nil?)
+    if(args[0].nil)
+      abort(@@usage)
+    end
+    
+    if(args[0].eql? "find_top_apps")
+      cmd = "find_top_apps"
+    elsif(args[0].eql? "find_apps_by_permission")
+      cmd = "find_apps_by_permission"
+    elsif(args[0].eql? "find_apps_with_permissions")
+      cmd = "find_apps_with_permissions"
+    else
+      puts "Error: Unknown command."
+      abort(@@usage)
+    end
+      
+    if(args[1].nil?)
       abort(@@usage)
     else
       out_dir = File.absolute_path(args[0])
-      start_main(out_dir)
+      start_main(out_dir, cmd)
     end
   end
 end
