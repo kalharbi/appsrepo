14d5b4b5b1d115b0821e13d9dc68d7ac24f5a164
Khalid Alharbi <Khalid3ahmed@gmail.com>
2014-06-14 12:39:22 -0600

    add version code and name

--- public_features/app.rb
+++ public_features/app.rb
@@ -2,22 +2,24 @@ require 'json'
 
 class App
   attr_reader :name
-  attr_accessor :title, :playStoreURL, :category, :price, :datePublished, :version, :operatingSystems, 
-  :ratingsCount, :rating, :contentRating, :creator, :creatorURL, :installSize, :installSizeText, 
-  :downloadsCount, :downloadsCountText, :description, :reviews, :permissions, :whatIsNew
+  attr_accessor :title, :playStoreURL, :category, :price, :datePublished,
+                :versionName, :versionCode, :operatingSystems, :ratingsCount,
+                :rating, :contentRating, :creator, :creatorURL, :installSize,
+                :installSizeText, :downloadsCount, :downloadsCountText,
+                :description, :reviews, :permissions, :whatIsNew
   
   def initialize(name)
     @name = name
   end
   
   def to_json
-    app_info = {'n' => @name, 't' => @title, 'desc' => @description, 
-        'url' => @playStoreURL, 'cat' => @category, 'pri' => @price, 
-        'dtp' => @datePublished, 'ver' => @version, 
-        'os' => @operatingSystems, 'rct' => @ratingsCount, 'rate' => @rating, 
-        'crat' => @contentRating, 'crt' => @creator, 'curl' => @creatorURL, 
-        'sz' => @installSize, 'sztxt' => @installSizeText, 
-        'dct' => @downloadsCount, 'dtxt' => @downloadsCountText, 
+    app_info = {'n' => @name, 't' => @title, 'desc' => @description,
+        'url' => @playStoreURL, 'cat' => @category, 'pri' => @price,
+        'dtp' => @datePublished, 'verc' => @versionCode, 'vern' => @versionName,
+        'os' => @operatingSystems, 'rct' => @ratingsCount, 'rate' => @rating,
+        'crat' => @contentRating, 'crt' => @creator, 'curl' => @creatorURL,
+        'sz' => @installSize, 'sztxt' => @installSizeText,
+        'dct' => @downloadsCount, 'dtxt' => @downloadsCountText,
         'per' => @permissions, 'new' => @whatIsNew, 'rev' => @reviews}
     JSON.parse(app_info.to_json)
   end
