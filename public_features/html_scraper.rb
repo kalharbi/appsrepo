67d39ac96036bf1a46a151d8ccd2a351b63df64c
Khalid Alharbi <Khalid3ahmed@gmail.com>
2014-06-15 12:07:01 -0600

    fix missing html file error

--- public_features/html_scraper.rb
+++ public_features/html_scraper.rb
@@ -13,7 +13,7 @@ class HtmlScraper
     begin
       page = Nokogiri::HTML(open(@html_file))
     rescue Errno::ENOENT
-      Logging.logger.error("Error: No such file or directory: #@html_file.")
+      Logging.logger.error("HTML file does not exist: #@html_file")
     end
   end
   
@@ -21,7 +21,8 @@ class HtmlScraper
   def get_what_is_new
     page = parse_html_file
     if(page.nil?)
-      return nil
+      Logging.logger.error("Could not read HTML file: #@html_file")
+      return []
     end
     new_changes_list = page.css('div.details-section-contents div.recent-change')
     changes = []
