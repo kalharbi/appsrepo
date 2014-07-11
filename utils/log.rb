7590a420b0ff5fc81760a47d8fef0e3534587b83
Khalid Alharbi <Khalid3ahmed@gmail.com>
2014-07-11 14:47:56 -0600

    set log file name dynamically

--- utils/log.rb
+++ utils/log.rb
@@ -4,25 +4,38 @@ require 'singleton'
 class Log
   include Singleton
   
+  @@file_name = nil
   attr_accessor :log_file_name
   
   def initialize
     # set file log name
-    @log_file_name = $PROGRAM_NAME + '-'  + Time.new.strftime("%Y-%m-%d") + ".log"
+    if @@file_name.nil?
+      @log_file_name = $PROGRAM_NAME + '-'  + Time.new.strftime("%Y-%m-%d") + ".log"
+    else
+      @log_file_name = @@file_name
+    end
     
     # only show "info" or higher messages on STDOUT using the Basic layout
     Logging.appenders.stdout(:level => :info)
     
-    # send all error log events to the file log as JSON
-    Logging.appenders.rolling_file(
-      @log_file_name,
-      :level  => :error,
-      :age    => 'daily',
-      :layout => Logging.layouts.json
-    )
+    begin
+      # send all error log events to the file log as JSON
+      Logging.appenders.rolling_file(
+        @log_file_name,
+        :level  => :error,
+        :age    => 'daily',
+        :layout => Logging.layouts.json
+        )
+    rescue ArgumentError => e
+      abort(e.message)
+    end
     @logger ||= Logging.logger['appsrepo']
     @logger.level = :info
     @logger.add_appenders @log_file_name, 'stdout'
+  end
+  
+  def self.log_file_name=log_file_name
+    @@file_name = log_file_name
   end
    
   def error(msg)
