d652420522def86c801395036d11cc6796bc8116
Khalid Alharbi <Khalid3ahmed@gmail.com>
2014-11-09 22:23:54 -0700

    escape special xml character entities

--- ui-graphml/src/main/java/org/bigdiff/graphmlwriters/GraphMLWriter.java
+++ ui-graphml/src/main/java/org/bigdiff/graphmlwriters/GraphMLWriter.java
@@ -1,125 +1,115 @@
 /**
-Khalid
+ Khalid
  */
 package org.bigdiff.graphmlwriters;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
-import java.util.Map;
-
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.util.Map;
+
 public class GraphMLWriter {
 
-	private final static Logger logger = LoggerFactory
-			.getLogger(GraphMLWriter.class);
-	private String fileName;
-	private Writer writer = null;
-	private String graphMLHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-			+ System.lineSeparator()
-			+ "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">"
-			+ System.lineSeparator()
-			+ "<graph id=\"G\" edgedefault=\"directed\">";
-	private String graphMLFooter = "</graph>" + System.lineSeparator()
-			+ "</graphml>";
+    private final static Logger logger = LoggerFactory
+            .getLogger(GraphMLWriter.class);
+    private String fileName;
+    private Writer writer = null;
+    private String graphMLHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + System.lineSeparator()
+            + "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">"
+            + System.lineSeparator()
+            + "<graph id=\"G\" edgedefault=\"directed\">";
+    private String graphMLFooter = "</graph>" + System.lineSeparator()
+            + "</graphml>";
 
-	public GraphMLWriter(String fileName) {
-		this.fileName = fileName;
-		String encoding = "utf-8";
-		try {
-			writer = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(this.fileName, false), encoding));
-			writer.write(this.graphMLHeader + System.lineSeparator());
-		} catch (UnsupportedEncodingException e) {
-			logger.error("encoding {} is not supported, {}", encoding, e);
-		} catch (FileNotFoundException e) {
-			logger.error("File Not found {}, {}", fileName, e);
-		} catch (IOException e) {
-			logger.error("IO Exception while writing to file: {}, {}",
-					fileName, e);
-		}
-	}
+    public GraphMLWriter(String fileName) {
+        this.fileName = fileName;
+        String encoding = "utf-8";
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(this.fileName, false), encoding));
+            writer.write(this.graphMLHeader + System.lineSeparator());
+        } catch (UnsupportedEncodingException e) {
+            logger.error("encoding {} is not supported, {}", encoding, e);
+        } catch (FileNotFoundException e) {
+            logger.error("File Not found {}, {}", fileName, e);
+        } catch (IOException e) {
+            logger.error("IO Exception while writing to file: {}, {}",
+                    fileName, e);
+        }
+    }
 
-	/**
-	 * Write a graph node (vertex) using the GraphML representation.
-	 * 
-	 * @param nodeId
-	 *            node id of the node in the beginning of the vertex
-	 *            representation.
-	 * @param dataMap
-	 *            a map of key/value pairs associated with the given graph
-	 *            nodeId element.
-	 */
-	public void writeNode(String nodeId, Map<Object, Object> dataMap) {
-		try {
-			writer.write("<node id=\"" + nodeId + "\">"
-					+ System.lineSeparator());
-			if (dataMap != null) {
-				for (Map.Entry<Object, Object> entry : dataMap.entrySet()) {
-					Object key = entry.getKey();
-					Object value = entry.getValue();
-					writer.write("<data key=\"" + key + "\">" + value
-							+ "</data>" + System.lineSeparator());
-				}
-			}
-			writer.write("</node>" + System.lineSeparator());
-		} catch (IOException e) {
-			logger.error("IO Exception while writing to file: {}, {}",
-					fileName, e);
-		}
-	}
+    /**
+     * Write a graph node (vertex) using the GraphML representation.
+     *
+     * @param nodeId  node id of the node in the beginning of the vertex
+     *                representation.
+     * @param dataMap a map of key/value pairs associated with the given graph
+     *                nodeId element.
+     */
+    public void writeNode(String nodeId, Map<Object, Object> dataMap) {
+        try {
+            writer.write("<node id=\"" + nodeId + "\">"
+                    + System.lineSeparator());
+            if (dataMap != null) {
+                for (Map.Entry<Object, Object> entry : dataMap.entrySet()) {
+                    Object key = entry.getKey().toString();
+                    Object value = entry.getValue().toString();
+                    writer.write("<data key=\"" + key + "\">" +
+                            StringEscapeUtils.escapeXml10(value.toString())
+                            + "</data>" + System.lineSeparator());
+                }
+            }
+            writer.write("</node>" + System.lineSeparator());
+        } catch (IOException e) {
+            logger.error("IO Exception while writing to file: {}, {}",
+                    fileName, e);
+        }
+    }
 
-	/**
-	 * Writes the graph edge that connects the source and target nodes together.
-	 * 
-	 * @param edgeId
-	 *            the edge Id
-	 * @param sourceId
-	 *            the identifier of the source node.
-	 * @param targetId
-	 *            the identifier of the target node.
-	 * @param label
-	 *            a label denoting the relationship represented by the edge.
-	 * @param dataMap
-	 *            a map of edge properties defined by a map from key to value.
-	 */
-	public void writeEdge(String edgeId, String sourceId, String targetId,
-			String label, Map<Object, Object> dataMap) {
-		try {
-			writer.write("<edge id=\"" + edgeId + "\" source=\"" + sourceId
-					+ "\" target=\"" + targetId + "\" label=\"" + label + "\">"
-					+ System.lineSeparator());
-			if (dataMap != null) {
-				for (Map.Entry<Object, Object> entry : dataMap.entrySet()) {
-					Object key = entry.getKey();
-					Object value = entry.getValue();
-					writer.write("<data key=\"" + key + "\">" + value
-							+ "</data>" + System.lineSeparator());
-				}
-			}
-			writer.write("</edge>" + System.lineSeparator());
+    /**
+     * Writes the graph edge that connects the source and target nodes together.
+     *
+     * @param edgeId   the edge Id
+     * @param sourceId the identifier of the source node.
+     * @param targetId the identifier of the target node.
+     * @param label    a label denoting the relationship represented by the edge.
+     * @param dataMap  a map of edge properties defined by a map from key to value.
+     */
+    public void writeEdge(String edgeId, String sourceId, String targetId,
+                          String label, Map<Object, Object> dataMap) {
+        try {
+            writer.write("<edge id=\"" + edgeId + "\" source=\"" + sourceId
+                    + "\" target=\"" + targetId + "\" label=\"" + label + "\">"
+                    + System.lineSeparator());
+            if (dataMap != null) {
+                for (Map.Entry<Object, Object> entry : dataMap.entrySet()) {
+                    Object key = entry.getKey();
+                    Object value = entry.getValue();
+                    writer.write("<data key=\"" + key + "\">" +
+                            StringEscapeUtils.escapeXml10(value.toString())
+                            + "</data>" + System.lineSeparator());
+                }
+            }
+            writer.write("</edge>" + System.lineSeparator());
 
-		} catch (IOException e) {
-			logger.error("IO Exception while writing to file: {}, {}",
-					fileName, e);
-		}
-	}
+        } catch (IOException e) {
+            logger.error("IO Exception while writing to file: {}, {}",
+                    fileName, e);
+        }
+    }
 
-	public void closeWrite() {
-		try {
-			writer.write(graphMLFooter + System.lineSeparator());
-			writer.close();
-		} catch (IOException e) {
-			logger.error(
-					"Failed to writer or close the writing stream of: {}, {}",
-					fileName, e);
-		}
-	}
+    public void closeWrite() {
+        try {
+            writer.write(graphMLFooter + System.lineSeparator());
+            writer.close();
+        } catch (IOException e) {
+            logger.error(
+                    "Failed to writer or close the writing stream of: {}, {}",
+                    fileName, e);
+        }
+    }
 
 }
