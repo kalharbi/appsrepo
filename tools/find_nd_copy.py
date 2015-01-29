f40f087e9e7aa20563b293bd6c659b356b470c73
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-01-29 15:34:23 -0700

    add find apk files in source directory
    add find files using both custom search and directly in the source
    directly.

--- tools/find_nd_copy.py
+++ tools/find_nd_copy.py
@@ -11,38 +11,42 @@ class FindNdCopy(object):
     
     log = logging.getLogger("find_nd_copy")
     log.setLevel(logging.DEBUG) # The logger's level must be set to the "lowest" level.
-    
+    # Flag that indicates the use of custom directory naming scheme. i.e. dir/c/com/a/amazon/com.amazon
+    use_custom_file_search = False
     def __init__(self):
         self.apk_files = []
         
     def start_main(self, apk_names_file, source_dir, target_dir):
         with open(apk_names_file, 'r') as f:
-            # skip the first line since it's the header line [apk_name, download_count]
+            # skip the first line since it's the header line [apk_name, version_code, download_count]
             next(f)
             for line in f:
                 arr = [items.strip() for items in line.split(',')]
                 apk_name = arr[0]
+                version_code = arr[1]
                 apk_path = source_dir
                 if apk_name:
                     # Only search in the directory of each apk file.
-                    for index, item in enumerate(apk_name.split('.')):
-                        apk_path = os.path.join(apk_path, item[0])
-                        apk_path = os.path.join(apk_path, item)
-                        if index == 1:
-                            break
-                    apk_path = os.path.join(apk_path, apk_name)
-                    apk_file = self.find_apk_file(apk_name, apk_path)
+                    if self.use_custom_file_search:
+                        for index, item in enumerate(apk_name.split('.')):
+                            apk_path = os.path.join(apk_path, item[0],item)
+                            if index == 1:
+                                break
+                        apk_path = os.path.join(apk_path, apk_name)
+                    else:
+                        apk_file = self.find_apk_file(apk_name, version_code, apk_path)
                     if apk_file:
                         # Copy the first file in the list
-                        # TODO: Handle multiple versions files.
                         self.copy_file(apk_file, target_dir)
                 
-    def find_apk_file(self, apk_name, source_directory):
+    def find_apk_file(self, apk_name, version_code, source_directory):
         if(not os.path.exists(source_directory)):
             self.log.error(source_directory + ' No such file or directory.')
             return None
-        
-        self.files_listing(os.path.abspath(source_directory), apk_name,'.apk')
+        if self.use_custom_file_search:
+            self.files_listing_custom_search(os.path.abspath(source_directory), apk_name, version_code,'.apk')
+        else:
+            self.files_listing(os.path.abspath(source_directory), apk_name, version_code,'.apk')
         found_apk_files = self.apk_files
         self.apk_files = []
         if len(found_apk_files) == 1:
@@ -63,14 +67,19 @@ class FindNdCopy(object):
             shutil.copy(apk_file, target_directory)
         except IOError as e:
             self.log.error(str(e))
-          
-    def files_listing(self, source, apk_name, ext_name):
+    
+    def files_listing(self, source, apk_name, version_code, ext_name):
+        apk_file = os.path.join(source, apk_name + '-' + version_code + ext_name)
+        if os.path.exists(apk_file):
+            self.apk_files.append(apk_file)
+              
+    def files_listing_custom_search(self, source, apk_name, version_code, ext_name):
         for item in os.listdir(source):
             file = os.path.join(source, item)
-            self.log.info('searching for ' + apk_name + ' in ' + file)
+            self.log.info('searching for ' + apk_name + '-' + version_code + ' in ' + file)
             if(os.path.isdir(file)):
-                self.files_listing(file, apk_name, ext_name)
-            elif(os.path.splitext(file)[1].lower() == ext_name and os.path.basename(os.path.splitext(file)[0]).startswith(apk_name)):
+                self.files_listing(file, apk_name, version_code, ext_name)
+            elif(os.path.splitext(file)[1].lower() == ext_name and os.path.basename(os.path.splitext(file)[0]).startswith(apk_name + '-' + version_code)):
                 self.apk_files.append(file)
     
     def main(self, args):
@@ -93,7 +102,8 @@ class FindNdCopy(object):
                           help="write logs to FILE.", metavar="FILE")
         parser.add_option('-v', '--verbose', dest="verbose", default=0,
                           action='count', help='Increase verbosity.')
-                          
+        parser.add_option('-c', '--custom', dest="custom_search", action='store_true', default=False,
+	                  help="search for apk files using the custom directory naming scheme. e.g, dir/c/com/a/amazon/com.amazon")
         (options, args) = parser.parse_args()
         if len(args) != 3:
             parser.error("incorrect number of arguments.")
@@ -110,7 +120,8 @@ class FindNdCopy(object):
             # set the file logger level if it exists
             if logging_file:
                 logging_file.setLevel(logging_level)
-        
+        if options.custom_search:
+	    self.use_custom_file_search = True
         # Get apk file names
         apk_names_file = None
         if os.path.isfile(args[0]):
