1034b656b537a150e5ec8318d1d7cf8e85b8b129
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-12 14:51:16 -0700

    remove root element label

--- ui-graphml/src/main/java/org/bigdiff/utils/Constants.java
+++ ui-graphml/src/main/java/org/bigdiff/utils/Constants.java
@@ -7,12 +7,10 @@ public class Constants {
 
     public final static String UI_GRAPHML_DIR = "ui-graphml";
     // Node Labels
-    public final static String ROOT = "ROOT";
     public final static String VIEW_GROUP = "VIEW_GROUP";
     public final static String VIEW = "VIEW";
 
     // Edge/Relationship Labels
-    public final static String HAS_ROOT = "HAS_ROOT_ELEMENT";
     public final static String HAS_VIEW_GROUP = "HAS_VIEW_GROUP";
     public final static String HAS_VIEW = "HAS_VIEW";
 
