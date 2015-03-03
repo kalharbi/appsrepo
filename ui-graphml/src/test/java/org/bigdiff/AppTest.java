24bf464670220a729ae83481ceb04f861f858330
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-03-03 15:52:31 -0700

    fix md5 comparison

--- ui-graphml/src/test/java/org/bigdiff/AppTest.java
+++ ui-graphml/src/test/java/org/bigdiff/AppTest.java
@@ -1,12 +1,14 @@
 package org.bigdiff;
 
 import org.junit.Before;
+import org.xml.sax.SAXException;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
-import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.junit.Assert.*;
 
 
@@ -27,7 +29,7 @@ public class AppTest {
     }
 
     @org.junit.Test
-    public void testGenerateGraphML() throws IOException {
+    public void testGenerateGraphML() throws IOException, SAXException {
         App app = new App();
         app.doGraphML(new File[]{apkDir});
         File uiGraphMLDir = new File(apkDir, "ui-graphml");
@@ -35,10 +37,16 @@ public class AppTest {
                 ".graphml");
         assertTrue("Failed to create the graphml file " + graphmlFile.getAbsolutePath(),
                 graphmlFile.exists());
-        // The size of the graphml file for me.pou.app version_code 188 should be ~ 9.1 KB
-        assertThat("GraphML " + graphmlFile.getAbsolutePath() + " is empty.",
-                graphmlFile.length(),
-                greaterThan(new Long(900)));
+        // The generated GraphML file should be exactly the same as the manually verified file.
+        File validGraphMLFile = new File(apkDir, "me.pou.app-188.graphml.valid");
+        FileInputStream fis_expected = new FileInputStream(validGraphMLFile);
+        String md5_validGraphMLFile = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis_expected);
+        fis_expected.close();
+        FileInputStream fis_actual = new FileInputStream(graphmlFile);
+        String md5_actualGraphMLFile = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis_actual);
+        fis_actual.close();
+        assertEquals("The generated graphML is invalid. MD5 comparison failed.",
+                md5_validGraphMLFile, md5_actualGraphMLFile);
     }
 
 }
