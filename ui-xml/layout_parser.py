6a6d4b79556374e0a0c483d5f56f5557bf322c23
Khalid Alharbi <khalid@MBA.local>
2015-05-24 12:35:36 -0600

    change code style

--- ui-xml/layout_parser.py
+++ ui-xml/layout_parser.py
@@ -4,8 +4,9 @@ from lxml import etree
 from xml.etree import ElementTree
 from lxml.etree import ParserError
 from lxml.etree import XMLSyntaxError
+
+
 class LayoutParser(object):
-    
     def __init__(self, log):
         self.log = log
 
@@ -24,12 +25,13 @@ class LayoutParser(object):
                 embedded_layout = child.get('layout')
                 if embedded_layout is None:
                     d = dict(child.attrib)
-                    for k,v in d.items():
+                    for k, v in d.items():
                         if '/' in v and '@' in v:
                             pattern = re.match(r'@(.*?)/', v)
                             if pattern is not None:
                                 dir_name = pattern.group(1)
-                                if os.path.exists(os.path.join(apk_dir, 'res', dir_name)):
+                                if os.path.exists(
+                                        os.path.join(apk_dir, 'res', dir_name)):
                                     embedded_layout = v
                                     break
                 if embedded_layout is None:
@@ -43,11 +45,13 @@ class LayoutParser(object):
                     embeded_dir = os.path.basename(os.path.dirname(layout_file))
                     embeded_file = embedded_layout
 
-                embeded_file_full_path = os.path.join(apk_dir, 'res', embeded_dir, 
+                embeded_file_full_path = os.path.join(apk_dir, 'res',
+                                                      embeded_dir,
                                                       embeded_file + '.xml')
                 # Get the tree of the included layout
-                emebeded_tree = LayoutParser(self.log).parse(embeded_file_full_path,
-                                                                apk_dir)
+                emebeded_tree = LayoutParser(self.log).parse(
+                    embeded_file_full_path,
+                    apk_dir)
                 if emebeded_tree is not None and child.getparent() is not None:
                     child.getparent().append(emebeded_tree.getroot())
                 if child.getparent() is not None:
@@ -55,7 +59,7 @@ class LayoutParser(object):
                     child.getparent().remove(child)
             else:
                 attributes = dict(child.attrib)
-                for attr_name, attr_value in attributes.iteritems(): 
+                for attr_name, attr_value in attributes.iteritems():
                     if '@' in attr_value:
                         pattern = re.match(r'@(.*?)/', attr_value)
                         if pattern is None:
@@ -66,7 +70,8 @@ class LayoutParser(object):
                                                 res_file_base_name)
                         res_attr = attr_value.split('/')[1]
                         if res_element != 'id' and res_element != '+id':
-                            ref_value = self.find_attr_value(res_file, res_element,
+                            ref_value = self.find_attr_value(res_file,
+                                                             res_element,
                                                              res_attr)
                             if ref_value is not None and len(ref_value) > 0:
                                 child.set(attr_name, ' '.join(ref_value))
@@ -76,17 +81,20 @@ class LayoutParser(object):
     def find_attr_value(self, xml_file, element_name, attribute_name):
         try:
             if not os.path.exists(xml_file):
-                self.log.warning('Warning: Resource file %s does not exist, ', xml_file)
+                self.log.warning('Warning: Resource file %s does not exist, ',
+                                 xml_file)
                 return []
             tree = etree.parse(xml_file)
             if attribute_name is not None:
                 xpath_query = "//" + element_name + '[@name="' + attribute_name + '"]/text()'
                 return tree.xpath(xpath_query,
-                  namespaces={'android': "http://schemas.android.com/apk/res/android"})
+                                  namespaces={
+                                      'android': "http://schemas.android.com/apk/res/android"})
         except (ParserError, XMLSyntaxError) as e:
-            self.log.error("XML parsing error occured while parsing file: %s", xml_file)
+            self.log.error("XML parsing error occured while parsing file: %s",
+                           xml_file)
         return []
-    
+
     def parse(self, layout_file, apk_dir):
         return self.start_parser(layout_file, apk_dir)
         
